81c548fb99cd9971f1270b37e1f1d617bb4f6bb4
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-13 17:12:32 +0530

    updated

---
+++ Basic/Arthimetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+int Add,Sub,Mul,Divide;
+
+a=5;
+b=5;	
+Add=a+b;
+printf("Add=%d\n",Add);
+Sub=a-b;
+printf("Sub=%d\n",Sub);
+Mul=a*b;
+printf("Mul=%d\n",Mul);
+Divide=a/b;
+printf("Divide=%d\n",Divide);
+	
+	
+}
