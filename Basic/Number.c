6504974e11e388081114b497c7dee0575e9734fa
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-17 16:02:17 +0530

    added number

---
+++ Basic/Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,e;
+    printf("Enter the start number");
+    scanf("%d",&n);
+
+    printf("Enter end number");
+    scanf("%d",&e);
+while(n<=e)
+{
+    printf("%d\n",n);
+    n++;
+}
+}
