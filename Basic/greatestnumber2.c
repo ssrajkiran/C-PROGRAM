eb5c6e0170dcf087ba74342d958a231fedc12b55
RAJKIRANSS <ssrajkiran1@gmail.com>
2021-05-12 08:16:03 +0530

    updated

---
+++ Basic/greatestnumber2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int a,b;
+	printf("Enter the number:");
+	scanf("%d",&a);
+	printf("Enter the number:");
+	scanf("%d",&b);
+
+	if(a>b){
+		printf("a is greater");}
+	else{
+	printf("b is greater");
+	}
+}
