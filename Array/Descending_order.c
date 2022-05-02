fe171c37caf026c51f5515b5caa4d4fc60e7c650
ssrajkiran <ssrajkiran1@gmail.com>
2022-05-02 10:02:10 +0530

    added Ascending and descending order

---
+++ Array/Descending_order.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main() {
+    
+   int a[10],n;
+   printf("Enter the value of n :");
+   scanf("%d",&n);
+   for(int i=0;i<n;i++){
+       printf("Enter the value of %d number :",i);
+       scanf("%d",&a[i]);
+    
+   }
+     printf("Descending order :\n");
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           if(a[i] > a[j]){
+               int tmp=a[i];
+               a[i]= a[j];
+               a[j]=tmp;
+           }
+       }
+   }
+   for(int i=0;i<n;i++){
+       printf("The value of %d number is %d\n",i,a[i]);
+   }
+  
+    return 0;
+}
